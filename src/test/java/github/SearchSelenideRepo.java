0e179b3e3f5dcbfb6ea3b19bff4144dc5c481972
DaymianDark <82811285+DaymianDark@users.noreply.github.com>
2021-12-22 20:09:22 +0300

    Update SearchSelenideRepo.java

--- src/test/java/github/SearchSelenideRepo.java
+++ src/test/java/github/SearchSelenideRepo.java
@@ -11,17 +11,38 @@ import static com.codeborne.selenide.WebDriverConditions.*;
 
 public class SearchSelenideRepo {
 
+    // стандартная структура теста выглядит так:
+    // ARRANGE (optional)
+    // ACT
+
+    // или так: с проверками после каждого теста    или так: с промежуточной проверкой в важных местах
+    // ARRANGE (optional)                           // ARRANGE (optional)
+    // ACT                                          // ACT
+    // ASSERT                                       // ACT
+    // ACT                                          // ACT
+    // ASSERT                                       // ASSERT
+    // ACT                                          // ACT
+    // ASSERT                                       // ASSERT
+
+    // вначале готовимся к тесту и проходим сценарий руками
+
     @Test
-    void shouldFindSelenideRepositoryInGithub(){
+    void shoudFindSelenideRepositoryInGithub() throws InterruptedException {
         // открыть страницу github.com
         open("https://github.com/");
         // ввести в поле поиска selenide и нажать Enter
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
         // нажимаем на линк от первого результата поиска
-        $$("ul.repo-list li").first().$("a").click();
+//        $$("ul.repo-list Li").first().click(); // selenide всегда кликает в самый центр элемента по этому не работает
+        $$("ul.repo-list Li").first().$("a").click();
         // check: в заголовке встречается selenide/selenide
+//        $("h1").shouldHave(text("selenide/selenide")); // не работает потому, что по логу текст selenide/selenide находится на трех строках
+        // можно добавить символы пропуска строк selenide\n/\nselenide или просто проставить пробелы
         $("h1").shouldHave(text("selenide / selenide"));
 
-        webdriver().shouldHave(url("saf"));
+//        sleep(5000);
+
+//        Все проверки НУЖНО делать через .should'ы!
+
     }
 }
