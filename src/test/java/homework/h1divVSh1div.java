3b960db9481648fcde8a3adfec2e3f3054ea9884
DaymianDark <nightkonn@gmail.com>
2021-12-22 23:18:24 +0300

    added 1 task

--- src/test/java/homework/h1divVSh1div.java
+++ src/test/java/homework/h1divVSh1div.java
@@ -11,9 +11,14 @@ public class h1divVSh1div {
     @Test
     void h1VSh1() throws InterruptedException {
 
-        open("https://github.com/selenide/selenide/wiki");
+        open("https://the-internet.herokuapp.com/dynamic_content");
+        System.out.println($(".row a"));
+        System.out.println($(".row").$("a"));
+
+
+//        open("https://github.com/selenide/selenide/wiki");
 //        System.out.println($("details[class='details-overlay details-reset position-relative'] div"));
-        System.out.println($("details[class='details-overlay details-reset position-relative']").$("div"));
+//        System.out.println($("details[class='details-overlay details-reset position-relative']").$("div"));
 
 //        sleep(10000);
     }
